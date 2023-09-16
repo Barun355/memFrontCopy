3c6b4782351fc64b174f4b25c0487772329c9a81
Barun355 <baruntiwary620@gmail.com>
2023-09-17 02:58:05 +0530

    Initial Commit

---
+++ src/components/ResultFrame1.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react'
+
+function ResultFrame1() {
+  return (
+		<div>
+			<div style={{backgroundColor:"white"}}>
+				<iframe
+					src="https://pbsiddharthaelectronics.in/results/results.html"
+					width="100%"
+					height="1500px"
+			
+				
+				></iframe>
+			</div>
+		</div>
+	);
+}
+
+export default ResultFrame1
---
+++ src/components/ResultFrame3.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react'
+
+function ResultFrame3() {
+  return (
+		<div>
+			<div>
+				<iframe
+					src="https://pbsc.siddharthaexam.in/Admin/stmtofmarks2021.php?Batch=2021"
+					width="100%"
+					height="1500px"
+				></iframe>
+			</div>
+			;
+		</div>
+	);
+}
+
+export default ResultFrame3
+
+
+
